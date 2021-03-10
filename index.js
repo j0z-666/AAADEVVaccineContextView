7131b1c2b5f2a74c513ac39cb43f9b1d7e8762ef
martinez71 <j_alberto_v1@hotmail.com>
2021-03-10 17:53:56 -0600

    second commit

--- index.js
+++ index.js
@@ -1,15 +1,38 @@
 const express = require('express');
+const axios = require('axios');
+var exphbs  = require('express-handlebars');
 const http = require('http');
 
 var app = express();
 server = http.createServer(app);
 
 app.use(express.static('public'));
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
 
 app.get('/citizenInfo', function(req, res) {
-    //res.sendFile(path.join(__dirname + '/index.html'));
-    console.log("app get /citizenInfo")
-    res.sendFile(__dirname+'/public/joz.json');
+
+    if(req.query.id == undefined){
+        res.send('Bad Request');
+        console.log('El request no tiene id');
+        return;
+    }
+    console.log("app get /citizenInfo, query id = " + req.query.id);
+
+    axios.get('https://breeze2-196.collaboratory.avaya.com/services/AAADEVContextStore/cs/contexts/'+req.query.id)
+    .then(function(resp){
+        if(resp.data.data != undefined && resp.data.data.valid != undefined){
+            let datos = resp.data.data;
+            datos.jsonStr = JSON.stringify(datos, undefined, 4);
+            res.render('ContextView', datos);
+        } else {
+            res.render('ContextView', {});
+        }
+    })
+    .catch(function(error){
+        console.log('Axios error: ' + error);
+        res.render('ContextView', {});
+    })
 });
 
 
